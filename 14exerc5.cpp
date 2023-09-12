246db4f4d8f3cee7a913776818ee1d7552799327
Luciano_Bernardes <lasbernardes@uesc.br>
2023-09-12 16:49:23 -0300

    bedroom lasbernardes

---
+++ 14exerc5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//#include<string>
+
+using namespace std;
+
+struct bedroom{
+    string cama;
+    string criado;
+    string guarda_roupa;
+    string poltrona;
+    string condicionador_ar;
+    string tv;
+    string estante;
+    string escrivaninha;
+    string mesa;
+    string comoda;
+    string ventilador;
+};
+
+int main(){
+    
+    return 0;
+}
